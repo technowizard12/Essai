25594f28e2bb5a62adccbbc6c44d044b7ec86f1c
Simon <technowizard12@gmail.com>
2011-02-24 20:34:36 -0800

    this time I actually added the source. Whoopsies

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Essai::Application.config.secret_token = '1bc151533a0522b336a9a02a701d537984a93d5435ce51557220dea9fa34be8686cb24f9962b9b74f3374a16522b0859c680b317303ddb8895e717329aed8852'
